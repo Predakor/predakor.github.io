283828a95fb431c747183e26c0871798df49110e
Predakor <patrykbusko@gmail.com>
2022-07-26 16:24:38 +0200

    Added routing and pages

---
+++ PersonalSite/src/pages/Index.tsx
@@ -0,0 +1,7 @@
+import HomePage from "./Home";
+import ProjectsPage from "./Projects";
+import ContactPage from "./Contact";
+
+export const Home = () => <HomePage />;
+export const Projects = () => <ProjectsPage />;
+export const Contact = () => <ContactPage />;
