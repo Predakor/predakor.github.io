bb1ffe513962eed45c758d32ac86f853acf053fc
Predakor <patrykbusko@gmail.com>
2022-08-14 14:41:34 +0200

    Added animationOnScroll and animate.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home, Projects, Contact } from "./pages/Index";
 import Layout from "./layouts";
 import "./index.css";
+import "animate.css";
 
 function App() {
   return (
--- src/components/LandingSection/LandingSection.tsx
+++ src/components/LandingSection/LandingSection.tsx
@@ -1,15 +1,27 @@
+import { AnimationOnScroll } from "react-animation-on-scroll";
 import styles from "./LandingSection.module.css";
 
 function LandingSection() {
   const age = new Date().getUTCFullYear() - 2002;
-
+  const animation = "animate__animated animate__";
+  const up = "fadeInUp";
+  const delay = "animate__delay-";
+  let c = 1;
+  const incrementDelay = () => `${delay + c++}s`;
   return (
     <section className={styles.landingSection}>
-      <h1 className={styles.landingHeader}>Hi I'm Patrick</h1>
-      <h2 className={styles.bigH2}>An aspiring front end developer</h2>
-      <p className={styles.p1}>I'am {age} years old front end developrt wannabie</p>
-      <p className={styles.p2}>Currently I'm looking for my first job</p>
-      <p className={styles.p3}>You can check my stack and projects</p>
+      <h1 className={`${styles.landingHeader} animate__animated animate__fadeInDownBig`}>
+        Hi I'm Patrick
+      </h1>
+
+      <AnimationOnScroll animateIn="animate_)">
+        <h2 className={`${styles.bigH2} ${animation}`}>An aspiring front end developer</h2>
+      </AnimationOnScroll>
+      <p className={`${animation + up} ${incrementDelay()}`}>I'am {age} years old front wannabie</p>
+      <p className={`${animation + up} ${incrementDelay()}`}>
+        Currently I'm looking for my first job
+      </p>
+      <p className={`${animation + up} ${incrementDelay()}`}>You can check my stack and projects</p>
     </section>
   );
 }
