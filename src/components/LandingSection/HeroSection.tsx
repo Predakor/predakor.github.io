f8f192bccd73282ab36e676fbbc1b4136e8835d8
Predakor <patrykbusko@gmail.com>
2023-06-11 12:59:25 +0200

    Removed React on scroll

--- src/components/LandingSection/HeroSection.tsx
+++ src/components/LandingSection/HeroSection.tsx
@@ -1,33 +1,48 @@
 import { motion } from "framer-motion";
 import styles from "./LandingSection.module.scss";
 
+const variants = {
+  hidden: (y = "-100%") => ({ y: y, opacity: 0 }),
+  slideIn: { y: 0, opacity: 1 },
+};
 function LandingSection() {
   return (
     <section className={styles.landingSection}>
       <motion.h1
-        animate={{ y: 0, opacity: 1 }}
-        initial={{ y: "-100%", opacity: 0 }}
+        variants={variants}
+        animate="slideIn"
+        initial={variants.hidden()}
         transition={{ duration: 0.5, type: "tween" }}
         className={`${styles.landingHeader}`}
       >
         Hi I'm Patrick
       </motion.h1>
 
-      <div className={styles.scrollHint}>
+      <motion.div
+        variants={variants}
+        initial={variants.hidden("-50%")}
+        animate={variants.slideIn}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className={styles.scrollHint}
+      >
         {/* rome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
         <motion.svg
           fillOpacity={0}
-          stroke="var(--tertiary-color)"
-          strokeWidth={"0.25em"}
           width={100}
           height={100}
-          animate={{ y: ["-25%", "0%"] }}
-          transition={{ duration: 1, repeat: Infinity, repeatType: "mirror" }}
+          viewBox={"-10 0 250 50"}
+          animate={{ y: ["-20%", "0%"] }}
+          transition={{
+            delay: 1,
+            duration: 1,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
         >
-          <motion.path d="M10,40 L50,90 L90,40" />
-          <motion.path initial={{ y: "-30%" }} d="M10,40 L50,90 L90,40" />
+          <motion.path d="M0,50 L50,100 100,50" />
+          <motion.path initial={{ y: "-100%" }} d="M0,50 L50,100 100,50" />
         </motion.svg>
-      </div>
+      </motion.div>
     </section>
   );
 }
