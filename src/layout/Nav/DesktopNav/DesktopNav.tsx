12af763b3fdc68a34795f9419b2fb19d8ece14c8
Predakor <patrykbusko@gmail.com>
2023-06-13 10:05:55 +0200

    Removed unnecessary dependencies

--- src/layout/Nav/DesktopNav/DesktopNav.tsx
+++ src/layout/Nav/DesktopNav/DesktopNav.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import { NavLink } from "react-router-dom";
 import navigationList from "../../../assets/navigationList";
 import styles from "./DesktopNav.module.scss";
 
--- src/layout/Nav/MobileNav/MobileNav.tsx
+++ src/layout/Nav/MobileNav/MobileNav.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import navigationList from "../../../assets/navigationList";
 import styles from "./MobileNav.module.scss";
 
