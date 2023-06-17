ff24ecd869e9bde85793c45996c57a6ddbb8d356
Predakor <patrykbusko@gmail.com>
2023-06-17 21:19:53 +0200

    Optimized images

--- src/components/ProjectCard/ProjectImage/ProjectImage.tsx
+++ src/components/ProjectCard/ProjectImage/ProjectImage.tsx
@@ -1,14 +1,11 @@
 const IMAGE_LINKS: { [key: string]: string } = {
-  tasker:
-    "https://user-images.githubusercontent.com/86599904/240371998-9ed3d278-a444-4ceb-aec7-b6f226bf5ee2.png",
-  typer:
-    "https://user-images.githubusercontent.com/86599904/240371998-9ed3d278-a444-4ceb-aec7-b6f226bf5ee2.png",
-  discofy:
-    "https://user-images.githubusercontent.com/86599904/244343364-091504a2-a4dc-4ddc-8cb5-0087658a7d53.png",
+  tasker: "/project/tasker.webp",
+  typer: "/project/typer.webp",
+  discofy: "/project/discofy.webp",
 };
 
 function Image({ projectName }: { projectName: string }) {
-  return <img src={IMAGE_LINKS[projectName]} alt={""} />;
+  return <img src={IMAGE_LINKS[projectName]} alt={""} loading={"lazy"} />;
 }
 
 export default Image;
